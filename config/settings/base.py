20ac22b99bd7385940619feff00e55afc71ad45a
waveFrontSet <paul.bubenzer@gmail.com>
2019-10-13 14:21:24 +0200

    Added permissions.

--- config/settings/base.py
+++ config/settings/base.py
@@ -265,4 +265,7 @@ SOCIALACCOUNT_ADAPTER = "django_recipe.users.adapters.SocialAccountAdapter"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
 }
